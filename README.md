ff147105f1c30f13568968dca2656a8b55879200
hyma2 <Hyma200@gmail.com>
2021-10-10 19:45:17 -0400

    init

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Getting Started with Create React App
-
+###### DEPLOYING A REACT APP: https://github.com/gitname/react-gh-pages
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/Countries.js
@@ -0,0 +1,48 @@
+export const countries = [];
+countries['CAN'] = ['USA'];
+countries['USA'] = ['CAN', 'MEX'];
+countries['MEX'] = ['USA', 'GTM', 'BLZ'];
+countries['BLZ'] = ['MEX', 'GTM'];
+countries['GTM'] = ['MEX', 'BLZ', 'SLV', 'HND'];
+countries['SLV'] = ['GTM', 'HND'];
+countries['HND'] = ['GTM', 'SLV', 'NIC'];
+countries['NIC'] = ['HND', 'CRI'];
+countries['CRI'] = ['NIC', 'PAN'];
+countries['PAN'] = ['CRI'];
+
+let isSubset = (largeArr, smallArr) => {
+    for (let i = 0; i<smallArr.length; i++){
+        if (largeArr.indexOf(smallArr[i]) === -1)
+            return false;
+    }
+    return true;
+}
+
+export function getPath(dst, src = 'USA'){
+    var visited = [src];
+    var finalPath = [src];
+    var currCountry = finalPath[0];
+    var i = 0;
+    var countryFound = countries[currCountry].includes(dst);
+    while(!countryFound || i<finalPath.length){
+        if (isSubset(visited, countries[currCountry])){
+            finalPath.splice(i, 1);
+            i--;
+        }
+        else if (!countryFound){
+            for (let j = 0; j<countries[currCountry].length; j++){
+                if (!visited.includes(countries[currCountry][j])){
+                    visited.push(countries[currCountry][j]);
+                    finalPath.push(countries[currCountry][j]);
+                }
+            }
+        }
+        currCountry = finalPath[++i];
+        if (currCountry === undefined)
+            break;
+        if (!countryFound)
+            countryFound = countries[currCountry].includes(dst);
+    }
+    finalPath.push(dst);
+    return finalPath;
+}
